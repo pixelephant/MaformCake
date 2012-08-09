3693f1d0c6d4d7b1dc8e4490b23a67460d56ae08
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-08-09 17:01:19 +0200

    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -114,7 +114,21 @@ $(document).ready(function(){
 	})
 
 	$("#offer-form").submit(function(){
-		$(this).find("input[type='submit']").css("background","green").attr("readonly","readonly").addClass("disabled").val("✓ Köszönjük.");
+		$.ajax({
+		  type: 'POST',
+		  url: "/email",
+			data: {name : $("#offer-name").val(),
+						email : $("#offer-email").val(),
+						budget : $("#offer-budget").val(),
+						description : $("#offer-description").val()
+						},
+			dataType: "json",
+		  success: function(resp){
+		  	console.log(resp.status);
+		  	if(resp.status == 'true'){
+		  		$("#offer-form").find("input[type='submit']").css("background","green").attr("readonly","readonly").addClass("disabled").val("✓ Köszönjük.");
+		  	}
+		}});
 		return false;
 	});
 
